74cd9f460387b32d4173b3c7d62a251be67865b8
Joel Marriott <joel.marriott.uk@gmail.com>
2022-08-20 21:47:02 +0100

    Create a new display and crudely display existing

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from tempfile import TemporaryFile
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from models import Display
 
-app = Flask(__name__)
+app = Flask('DIB')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dib.db'
 db = SQLAlchemy(app)
 
@@ -9,6 +11,23 @@ db = SQLAlchemy(app)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    if request.method == 'POST':
+        print(str(request.form))
+        new_display = Display(**request.form)
+
+        try:
+            db.session.add(new_display)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your display'
+    else:
+        displays = Display.query.order_by(Display.date_created).all()
+        return render_template('newdisplay.html', displays=displays)
 
 
 if __name__ == "__main__":
---
+++ models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask('DIB')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dib.db'
+db = SQLAlchemy(app)
+
+class Display(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    street1 = db.Column(db.String(200), nullable=False)
+    street2 = db.Column(db.String(200), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    postcode = db.Column(db.String(8), nullable=False)     
+    latitude = db.Column(db.String(8), nullable=False)     
+    longitude = db.Column(db.String(8), nullable=False)     
+    method = db.Column(db.String(200), nullable=False)
+    photos = db.Column(db.String(200), nullable=False)
+    link = db.Column(db.String(200), nullable=False)
+    type = db.Column(db.String(20), nullable=False)
+    info = db.Column(db.String(1000), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Display %r>'%self.id
